b0c10f47bea6ffc64258f0d1af08f0378533db81
Rasmusbaywendt <Rasmusbaywendt@gmail.com>
2021-04-21 13:21:14 +0200

    header.php

--- wp-content/themes/gruppe-child/header.php
+++ wp-content/themes/gruppe-child/header.php
@@ -10,29 +10,35 @@
  * @subpackage Twenty_Nineteen
  * @since Twenty Nineteen 1.0
  */
-?><!doctype html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="profile" href="https://gmpg.org/xfn/11" />
-	<?php wp_head(); ?>
-</head>
+?>
+    <!doctype html>
+    <html <?php language_attributes(); ?>>
 
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentynineteen' ); ?></a>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="profile" href="https://gmpg.org/xfn/11" />
+        <?php wp_head(); ?>
+    </head>
 
-		<header id="masthead" class="<?php echo is_singular() && twentynineteen_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header'; ?>">
+    <body <?php body_class(); ?>>
+        <?php wp_body_open(); ?>
+            <div id="page" class="site">
+                testtekst
+                <a class="skip-link screen-reader-text" href="#content">
+                    <?php _e( 'Skip to content', 'twentynineteen' ); ?>
+                </a>
 
-			<div class="site-branding-container">
-				<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
-			</div><!-- .site-branding-container -->
+                <header id="masthead" class="<?php echo is_singular() && twentynineteen_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header'; ?>">
 
-			<?php if ( is_singular() && twentynineteen_can_show_post_thumbnail() ) : ?>
-				<div class="site-featured-image">
-					<?php
+                    <div class="site-branding-container">
+                        <?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
+                    </div>
+                    <!-- .site-branding-container -->
+
+                    <?php if ( is_singular() && twentynineteen_can_show_post_thumbnail() ) : ?>
+                        <div class="site-featured-image">
+                            <?php
 						twentynineteen_post_thumbnail();
 						the_post();
 						$discussion = ! is_page() && twentynineteen_can_show_post_thumbnail() ? twentynineteen_get_discussion_data() : null;
@@ -42,12 +48,14 @@
 						$classes = 'entry-header has-discussion';
 					}
 					?>
-					<div class="<?php echo $classes; ?>">
-						<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
-					</div><!-- .entry-header -->
-					<?php rewind_posts(); ?>
-				</div>
-			<?php endif; ?>
-		</header><!-- #masthead -->
+                                <div class="<?php echo $classes; ?>">
+                                    <?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
+                                </div>
+                                <!-- .entry-header -->
+                                <?php rewind_posts(); ?>
+                        </div>
+                        <?php endif; ?>
+                </header>
+                <!-- #masthead -->
 
-	<div id="content" class="site-content">
+                <div id="content" class="site-content">
