9720b87005ed82a6df88b16628558f386accedb7
Ziax0170 <ziax0170@stud.kea.dk>
2021-04-23 09:25:43 +0200

    Update page-poscast.php

--- wp-content/themes/gruppe-child/page-poscast.php
+++ wp-content/themes/gruppe-child/page-poscast.php
@@ -12,21 +12,6 @@
 get_header();
 ?>
 
-<?php
-
-			// Start the Loop.
-			while ( have_posts() ) :
-				the_post();
-
-				get_template_part( 'template-parts/content/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
-
-			endwhile; // End the loop.
-			?>
 			
  <!--  her starter min kode-->
  
@@ -102,9 +87,6 @@ get_header();
 </section> <!-- #primary -->
  
 		
-
-			
-
 
 <?php
 get_footer();
